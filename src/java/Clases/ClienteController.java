1ba606e92cae78b33181f3c247f479971da8b35a
Brandon <bampereza@miumg.edu.gt>
2022-08-13 09:48:31 -0600

    Brandon_primerParcial5

--- src/java/Clases/ClienteController.java
+++ src/java/Clases/ClienteController.java
@@ -10,4 +10,21 @@ package Clases;
  */
 public class ClienteController {
     
+       Cliente[] tablaCliente;
+       int indiceArray;
+    
+    public ClienteController(){
+        this.tablaCliente = new Cliente[100];
+        this.indiceArray=0;
+    }
+    
+     public void guardarCliente(Cliente cliente){
+        this.tablaCliente[this.indiceArray]=cliente;  
+        this.indiceArray=this.indiceArray+1;
+    }
+    
+    public Cliente[] getCliente(){
+        return this.tablaCliente;
+    }
+    
 }
